125b59aba30c06e2304033fd5cb2961c47e78783
Robert Haines <rhaines@manchester.ac.uk>
2025-02-02 18:35:19 +0000

    Add a test to ensure correct version number format.

---
+++ test/models/zip/bzip2/version_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class VersionTest < MiniTest::Test
+  def test_version
+    # Ensure all our versions numbers have at least MAJOR.MINOR.PATCH
+    # elements separated by dots, to comply with Semantic Versioning.
+    assert_match(/^\d+\.\d+\.\d+/, Zip::Bzip2::VERSION)
+  end
+end
